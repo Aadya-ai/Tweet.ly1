11edf08fd40e18935cff694eedd12747269c47c9
Candy Crush <76581343+Aadya-ai@users.noreply.github.com>
2021-06-06 22:36:48 +0530

    Update home_page.js

--- home_page.js
+++ home_page.js
@@ -1,5 +1,5 @@
 function addUser() {
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("USERNAME" , user_name );
-    window.location.href = "";
+    window.location.href = "https://mohit-coder99.github.io/tweet.ly/rooms.html";
 }
